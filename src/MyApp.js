c3a1b0967988752abed42567f77cf3c0a4396c55
ericta-portfolio <ericcta@gmail.com>
2021-01-09 22:55:10 -0800

    Added Forms

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Table from './Table'
+import Form from './Form';
 
 function MyApp() {
    const [characters, setCharacters] = useState([
@@ -34,6 +35,7 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   )
 }
